94477f91a255b084e447b6739f2b2377f0176de5
unknown <alessandropirela@gmail.com>
2026-01-20 15:22:07 -0700

    Week 2

---
+++ app/week-2/student-info.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Alessandro</h1>
+      <Link href={"https://github.com/Alessandro195/cprg306-assignments"}>github</Link>
+    </main>
+  );
+}
